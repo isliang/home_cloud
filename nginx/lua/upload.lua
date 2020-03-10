14b4517363eb6035dac3495c32d73171b3f85c48
isliang <yesuhuangsi@163.com>
2020-03-10 17:29:51 +0800

    Update upload.lua

--- nginx/lua/upload.lua
+++ nginx/lua/upload.lua
@@ -1,12 +1,17 @@
 local upload = require "resty.upload"
 
 local chunk_size = 4096
-local form = upload:new(chunk_size)
+local form, err = upload:new(chunk_size)
+if not form then
+    ngx.say("CODE: ", ngx.ERR, ", MSG: failed to new upload: ", err)
+    return
+end
 local file
 
 function get_file_name(res)
-    for file_name in string.gmatch(res[2], 'filename="(.+)"') do
-        return ngx.var.store_dir .. file_name
+    local filename = ngx.re.match(res,'(.+)filename="(.+)"(.*)')
+    if filename then
+        return ngx.var.store_dir .. filename[2]
     end
 end
 
@@ -19,15 +24,19 @@ while true do
     end
 
     if typ == "header" then
-        local file_name = get_file_name(res)
-        if file_name then
-            file = io.open(file_name, "w+")
-            if not file then
-                ngx.say("failed to open file ", file_name)
+        if res[1] == "Content-Disposition" then
+            local file_name = get_file_name(res[2])
+            if file_name then
+                file = io.open(file_name, "w+")
+                if not file then
+                    ngx.say("failed to open file ", file_name)
+                    return
+                end
+            else
+                ngx.say("failed to parse filename")
                 return
             end
         end
-
     elseif typ == "body" then
         if file then
             file:write(res)

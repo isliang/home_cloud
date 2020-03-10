58ad2e1f15f3572c6ba9f4ae9e29f5f88a43a552
isliang <yesuhuangsi@163.com>
2020-03-10 16:15:29 +0800

    Update upload.lua

--- nginx/lua/upload.lua
+++ nginx/lua/upload.lua
@@ -1,83 +1,45 @@
--- upload.lua
---==========================================
--- 文件上传
---==========================================
 local upload = require "resty.upload"
-local cjson = require "cjson"
+
 local chunk_size = 4096
-local form, err = upload:new(chunk_size)
-if not form then
-    ngx.log(ngx.ERR, "failed to new upload: ", err)
-    ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
+local form = upload:new(chunk_size)
+local file
+
+function get_file_name(res)
+    for file_name in string.gmatch(res[2], 'filename="(.+)"') do
+        return ngx.var.store_dir .. file_name
+    end
 end
-form:set_timeout(1000)
--- 字符串 split 分割
-string.split = function(s, p)
-    local rt= {}
-    string.gsub(s, '[^'..p..']+', function(w) table.insert(rt, w) end )
-    return rt
-end
--- 支持字符串前后 trim
-string.trim = function(s)
-    return (s:gsub("^%s*(.-)%s*$", "%1"))
-end
--- 文件保存的根路径
-local saveRootPath = ngx.var.store_dir
--- 保存的文件对象
-local fileToSave
---文件是否成功保存
-local ret_save = false
+
 while true do
     local typ, res, err = form:read()
+
     if not typ then
         ngx.say("failed to read: ", err)
         return
     end
+
     if typ == "header" then
-        -- 开始读取 http header
-        -- 解析出本次上传的文件名
-        local key = res[1]
-        local value = res[2]
-        if key == "Content-Disposition" then
-            -- 解析出本次上传的文件名
-            -- form-data; name="testFileName"; filename="testfile.txt"
-            local kvlist = string.split(value, ';')
-            for _, kv in ipairs(kvlist) do
-                local seg = string.trim(kv)
-                if seg:find("filename") then
-                    local kvfile = string.split(seg, "=")
-                    local filename = string.sub(kvfile[2], 2, -2)
-                    if filename then
-                        fileToSave = io.open(saveRootPath .. filename, "w+")
-                        if not fileToSave then
-                            ngx.say("failed to open file ", filename)
-                            return
-                        end
-                        break
-                    end
-                end
+        local file_name = get_file_name(res)
+        if file_name then
+            file = io.open(file_name, "w+")
+            if not file then
+                ngx.say("failed to open file ", file_name)
+                return
             end
         end
+
     elseif typ == "body" then
-        -- 开始读取 http body
-        if fileToSave then
-            fileToSave:write(res)
-        end
-    elseif typ == "part_end" then
-        -- 文件写结束，关闭文件
-        if fileToSave then
-            fileToSave:close()
-            fileToSave = nil
+        if file then
+            file:write(res)
         end
 
-        ret_save = true
+    elseif typ == "part_end" then
+        file:close()
+        file = nil
     elseif typ == "eof" then
-        -- 文件读取结束
+        ngx.say("upload success!")
         break
     else
-        ngx.log(ngx.INFO, "do other things")
+        -- do nothing
     end
-end
-if ret_save then
-    ngx.say("save file ok")
 end

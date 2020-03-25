5643b9b97fb12df232f93da000b862fd5d1c8063
isliang <yesuhuangsi@163.com>
2020-03-25 10:29:27 +0800

    Update access-token.lua

--- nginx/lua/access-token.lua
+++ nginx/lua/access-token.lua
@@ -1,13 +1,8 @@
 local arg = ngx.req.get_uri_args()
-local params = {}
-for k,v in pairs(arg) do
-    params[k] = v
-end
-
-
-if (ngx.var.cookie_token ~= "123") then
-    if (params["token"] == "123") then
-        ngx.header['Set-Cookie'] = 'token=123; path=/; Expires=' .. ngx.cookie_time(ngx.time() + 60 * 30)
+local token = "123"
+if (ngx.var.cookie_token ~= token) then
+    if (arg["token"] == token) then
+        ngx.header['Set-Cookie'] = 'token=' .. token .. '; path=/; Expires=' .. ngx.cookie_time(ngx.time() + 60 * 30)
     else
         ngx.exit(ngx.HTTP_BAD_REQUEST)
     end
